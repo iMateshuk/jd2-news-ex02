cc98cd75953c5f5fc35804ec3b66ae72498b9e80
iMateshuk <80713398+iMateshuk@users.noreply.github.com>
2021-07-06 15:17:56 +0300

    Delete news/src/main directory

--- news/src/main/java/by/http/news/controller/CommandName.java
+++
@@ -1,8 +0,0 @@
-package by.http.news.controller;
-
-public enum CommandName {
-	
-	REGISTRATION, AUTHORIZATION, USER_ACTION, ANSWER, INDEX, USER_AUTHORIZATION, USER_REGISTRATION, 
-	UNKNOWN_COMMAND
-
-}
